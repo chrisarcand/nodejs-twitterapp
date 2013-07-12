3a73e4350ab6107a8c11d572d377a8e2f8ca37e9
Chris Arcand <chris@chrisarcand.com>
2013-07-12 16:32:29 -0500

    Working oauth callback with Twitter

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-var http = require("http")
-  , express = require("express")
+var http = require('http')
+  , express = require('express')
   , OAuth= require('oauth').OAuth
+  , request = require('request')
+  , querystring = require('querystring')
   , routes = require('./routes')
   , keys = require('./twitterkeys')
   , config = require('./config.js')
@@ -13,34 +15,67 @@ var app = express();
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.use(express.cookieParser("dRewabraPhUxes9akemaPheW"));
+  app.use(express.session());
 });
 
-/* ROUTES */
-app.get('/', routes.index);
+var callback = 'http://localhost:' + config.port + '/callback';
 
+var getTwitter = new OAuth('https://api.twitter.com/oauth/request_token', 
+                   'https://api.twitter.com/oauth/access_token', 
+                   keys.consumer_key,
+                   keys.consumer_secret, 
+                   '1.0',
+                   callback, 
+                   'HMAC-SHA1');
+
+/* ROUTES */
+
+app.get('/', function(req, res) {
+  console.log(req.session.oauth);
+  res.render('index', { title: 'My Twitter App' });
+});
+
+app.get('/login', function(req, res) {
+  
+    getTwitter.getOAuthRequestToken(function(error, token, secret, results) {
+        if (error) {
+            console.log(error);
+        } else {
+            req.session.oauth = {};
+            req.session.oauth.token = token;
+            req.session.oauth.token_secret = secret;
+
+            res.redirect('https://twitter.com/oauth/authenticate?oauth_token=' + token);
+        }
+    });
+});
+
+app.get('/callback', function(req, res, next) {
+
+    if (req.session.oauth !== undefined) {
+        req.session.oauth.verifier = req.query.oauth_verifier;
+        var oauth = req.session.oauth;
+        getTwitter.getOAuthAccessToken(oauth.token, oauth.token_secret, oauth.verifier, function(error, token, secret, results) {
+            if (error) {
+                console.log(error);
+            } else {
+                req.session.oauth.access_token = token;
+                req.session.oauth.access_token_secret = secret;
+                req.session.oauth.screen_name = results.screen_name;
+
+                console.log(results);
+                console.log('User authenticated to Twitter');
+                
+                res.redirect('/');
+            }
+        });
+    } else {
+        res.redirect('/');
+    }
+});
 
 
 
 app.listen(config.port);
 console.log('Server started on port ' + config.port);
-
-
-/*var oauth = new OAuth(
-  'https://api.twitter.com/oauth/request_token',
-  'https://api.twitter.com/oauth/access_token',
-  keys.consumer_key,
-  keys.consumer_secret,
-  '1.0A',
-  'http://localhost:8888/oauth/callback',
-  'HMAC-SHA1'
-);
-
-oauth.get(
-  'https://api.twitter.com/1.1/statuses/home_timeline.json',
-  keys.access_token, 
-  keys.token_secret,             
-  function (e, data, res){
-    if (e) console.error(e);        
-    logAsJSON(data);    
-  }
-*/
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,6 @@
+/* ROUTES WILL BE CONTROLLED IN APP.JS FOR NOW */
+
+/*
 exports.index = function(req, res){
   res.render('index', { title: 'My Twitter App' });
-};
+};*/
