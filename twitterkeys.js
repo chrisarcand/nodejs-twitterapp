2d64e86d6f610db1b90345e558213e24019e492b
Chris Arcand <chris@chrisarcand.com>
2013-07-14 22:36:58 -0500

    Keys note

--- twitterkeys.js
+++ twitterkeys.js
@@ -1,3 +1,6 @@
+/* Yes, yes, I know. Before you start drooling over my token secrets, wanting to cause trouble:
+Remember that it's a dummy account and I don't care too much right now.*/
+
 exports.consumer_key = "RaykYeRTqMQQwqZMTsP7nA";
 exports.consumer_secret = "tW2X4aykFMYL715sjIV1vjwjlYmhcpLsJ4gOHYbNs";
 exports.access_token = '1584636296-EbNbVu8n5GqBVWgbpN7iPwtqGzCSYsbbUmxLOXm';
